dcf8600702c732c9f7f784f93d8e17a43f65d20f
lucas.henrique2@fatec.sp.gov.br <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>
2022-01-19 23:24:38 -0300

    Initial commit

--- .exercicios/ex_usuario_senha.py
+++ .exercicios/ex_usuario_senha.py
@@ -1,8 +1,8 @@
 nome = input('Digite o nome: ').strip().upper()
-
 senha = input('Digite uma senha alfabética: ').strip().upper()
+print(senha in nome)
 
-while nome in senha:
+while senha in nome:
     print('DIGITE UMA SENHA DIFERENTE DO NOME!!')
-    nome = [input('Digite o nome: ').strip().upper()]
-    senha = [input('Digite uma senha alfabética: ').strip().upper()]
+    nome = input('Digite o nome: ').strip().upper()
+    senha = input('Digite uma senha alfabética: ').strip().upper()
